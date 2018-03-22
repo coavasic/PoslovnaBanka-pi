cbcb1e46bd393d3d435af52e581ce25f24ae2311
Aleksandar Vasic <coavasic96@gmail.com>
2018-03-22 14:40:21 +0100

    Initial commit

---
+++ src/main/java/poslovna/informatika/banka/PoslovnaBanka/PoslovnaBankaApplication.java
@@ -0,0 +1,12 @@
+package poslovna.informatika.banka.PoslovnaBanka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoslovnaBankaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoslovnaBankaApplication.class, args);
+	}
+}
